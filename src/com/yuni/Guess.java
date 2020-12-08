02c8b11ca531dd4411cba53ff5c489a131b1226d
409570157 <75302069+409570157@users.noreply.github.com>
2020-12-08 15:46:35 +0800

    Update Guess.java

--- src/com/yuni/Guess.java
+++ src/com/yuni/Guess.java
@@ -1,10 +1,29 @@
 package com.yuni;
 
+import java.util.Random;
+
 public class Guess {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Random r = new Random();
+		char[] secret = new char[4];
+		for(int i=0;i<4;i++) {
+			char a = (char)(r.nextInt(10)+48);
+			
+		Boolean d = false;
+		for(int c=0;c<i;c++) {
+			if (secret[c] == a ) {
+				
+				d = true;
+				break;
+			}}if(d) {
+				i--;
+			}else {
+				System.out.println(a);
+				secret[i]= a;
+			
+		}	
 	}
 
-}
+}}
